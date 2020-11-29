bd39280a7dacb8da591f1ec39714aae13461af0d
LeeoSilva <leeosilva0909@gmail.com>
2020-11-29 05:10:28 -0300

    added new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import Home from '../pages/Home';
-import Products from '../pages/Products';
-import Categories from '../pages/Categories';
 
+import Products from '../pages/Products';
+import EditProduct from '../pages/EditProduct'; 
+import CreateProduct from '../pages/CreateProduct'; 
+
+import Categories from '../pages/Categories';
+import EditCategory from '../pages/EditCategory'; 
+import CreateCategory from '../pages/CreateCategory'; 
 
 export default class ApplicationRoutes extends Component {
     render(){
         return(
             <Router>
                 <Route path="/" exact component={Home}/>
+
                 <Route path="/products" exact component={Products}/>
+                <Route path="/products/add" exact component={CreateProduct}/>
+                <Route path="/products/edit/:uuid" exact component={EditProduct}/>
+
                 <Route path="/categories" exact component={Categories}/>
+                <Route path="/categories/add" exact component={CreateCategory}/>
+                <Route path="/categories/edit/:uuid" exact component={EditCategory}/>
             </Router>
         );
     }
